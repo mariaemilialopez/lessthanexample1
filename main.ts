f2119429e291cc29bec3eaefb4c082eb4018da6a
mariaemilialopez <113448146+mariaemilialopez@users.noreply.github.com>
2022-10-18 08:49:59 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,53 @@
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, otherSprite) {
+    game.over(false)
+})
+sprites.onDestroyed(SpriteKind.Player, function (sprite) {
+    info.changeScoreBy(1)
+})
+let projectile: Sprite = null
+let mySprite = sprites.create(img`
+    ..............ffffff....
+    ..fc.........fcc88ff....
+    ..f6c.....fffccccff.....
+    ..f66ccccc88888888cc....
+    ..f866ccc888886668b9c...
+    ..cf86888888888866999c..
+    .ccf8888888888888999b8c.
+    fc88cc88888888b1111b888c
+    f88ccccccc8888991188888f
+    ffffffc888c88888888888f.
+    ....ff888866c8888888ff..
+    ...cf888866fffffffff....
+    ...c888866ffc8f.........
+    ...c8888cfffccf.........
+    ...ffffffff8cf..........
+    ........fff8c...........
+    `, SpriteKind.Player)
+controller.moveSprite(mySprite, 0, 50)
+mySprite.x = 8
+mySprite.setStayInScreen(true)
+info.setScore(0)
+game.onUpdateInterval(500, function () {
+    projectile = sprites.createProjectileFromSide(img`
+        . . . . . . . b b . . . . . . . 
+        . . . . . . b d d b . . . . . . 
+        . . . . . b d 5 5 d b . . . . . 
+        . . . . b b 5 5 5 5 b b . . . . 
+        . . . . b 5 5 5 5 5 5 b . . . . 
+        b b b b b 5 5 5 5 1 1 d b b b b 
+        b 5 5 5 5 5 5 5 5 1 1 1 5 5 5 b 
+        b d d 5 5 5 5 5 5 1 1 1 5 d d b 
+        . b d d 5 5 5 5 5 5 5 5 d d b . 
+        . . b b 5 5 5 5 5 5 5 5 b b . . 
+        . . c b 5 5 5 5 5 5 5 5 b c . . 
+        . . c 5 5 5 5 d d 5 5 5 5 c . . 
+        . . c 5 5 d b b b b d 5 5 c . . 
+        . . c 5 d b c c c c b d 5 c . . 
+        . . c c c c . . . . c c c c . . 
+        . . . . . . . . . . . . . . . . 
+        `, -150, 0)
+    projectile.setPosition(scene.screenWidth(), randint(0, scene.screenHeight()))
+    if (info.score() < 10) {
+        projectile.vx += 100
+    }
+})
